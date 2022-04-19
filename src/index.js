cb19f0f5d2e7efe14126ef5c5c87904fc2a2f75d
Collins <mutaic84@gmail.com>
2022-04-19 23:00:30 +0300

    display beer information

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const beerList = document.getElementById("beer-list");
     const beerName = document.getElementById("beer-name");
-    const beerImage = document.getElementById("beer-image");
+    const beerImage = document.querySelector("img");
     const beerDescription = document.getElementById("beer-description");
     const editDescriptionForm = document.getElementById("description-form");
     const customerReviews = document.getElementById("review-list");
@@ -9,16 +9,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fetch("http://localhost:3000/beers")
     .then(response => response.json())
-    .then(data => {
+    .then(beerData => {
         beerList.innerHTML = ''
-        data.forEach(item => {
+        beerName.innerText = beerData[0].name
+        beerImage.setAttribute("src", beerData[0].image_url)
+        beerDescription.innerText = beerData[0].description
+        beerData.forEach(item => {
             createDOM(item)
         })
     })
 
-    function createDOM(data) {
-        let item = document.createElement("li")
-        item.innerText = data.name
-        beerList.appendChild(item)
+    function createDOM(beerDataItem) {
+        let beer = document.createElement("li")
+        beer.addEventListener("click", function() {
+            displayBeerInfo(beerDataItem)
+        })
+        beer.innerText = beerDataItem.name
+        beerList.appendChild(beer)
+
+    }
+
+    
+
+    function displayBeerInfo(beer) {
+        beerName.innerText = beer.name
+        beerImage.setAttribute("src", beer.image_url)
+        beerDescription.innerText = beer.description
     }
 })
