4f4d3e65897e7ccd51981f145eb9b25584d87736
Collins <mutaic84@gmail.com>
2022-04-19 20:02:51 +0300

    get DOM elements for manipulation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// Code here
+document.addEventListener("DOMContentLoaded", () => {
+    const beerList = document.getElementById("beer-list");
+    const beerName = document.getElementById("beer-name");
+    const beerImage = document.getElementById("beer-image");
+    const beerDescription = document.getElementById("beer-description");
+    const editDescriptionForm = document.getElementById("description-form");
+    const customerReviews = document.getElementById("review-list");
+    const reviewForm = document.getElementById("review-form")
+})
